17f9c22bbd6dbec1d4f294e3e1aa7830c29462bd
Larry Burris III <larry.n.burris@gmail.com>
2016-10-19 02:53:37 -0400

    Add Untrusted and update description on Optional

--- src/LL.NET.Security/LL.NET.Security.Core/Optional.cs
+++ src/LL.NET.Security/LL.NET.Security.Core/Optional.cs
@@ -1,6 +1,11 @@
 ﻿namespace LL.NET.Security.Core
 {
-    //Generic class for enforcing validation on nullable objects.
+    /// <summary>
+    /// Generic class for enforcing validation on nullable objects in the anticorruption layer.
+    /// Note: Do not use this in your domain objects. It is no more beneficial than an object that
+    /// can be null once it's in your domain.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public class Optional<T>
     {
         public bool IsPresent { get; }
---
+++ src/LL.NET.Security/LL.NET.Security.Core/Untrusted.cs
@@ -0,0 +1,26 @@
+﻿namespace LL.NET.Security.Core
+{
+    /// <summary>
+    /// This is container that prevents the value inside from being accessed unless
+    /// it has been validated. The following line makes the <see cref="Value"/>
+    /// parameter accessible from the Validation libary.
+    /// [assembly: InternalIsVisibleTo("Validation")]
+    /// This forces developers to ensure their data has been validated by only
+    /// allowing access through validators.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class Untrusted<T>
+    {
+        readonly T _value;
+
+        public Untrusted(T value)
+        {
+            _value = value;
+        }
+
+        private T Value
+        {
+            get { return _value; }
+        }
+    }
+}

72df02101c089b90d630038980dad26217de9617
Larry Burris III <larry.n.burris@gmail.com>
2016-10-19 02:26:15 -0400

    Add LL.NET.Security.Core and Optional class

---
+++ src/LL.NET.Security/LL.NET.Security.Core/Optional.cs
@@ -0,0 +1,9 @@
+﻿namespace LL.NET.Security.Core
+{
+    //Generic class for enforcing validation on nullable objects.
+    public class Optional<T>
+    {
+        public bool IsPresent { get; }
+        public T Get { get; }
+    }
+}
